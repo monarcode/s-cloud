519a83fb03d9e05b5c67bd13610a7e41c880ad22
monarcode <ugbahisioma@gmail.com>
2020-10-05 11:28:27 +0100

    fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-      let srcl = $(window).scrollTop();
+$(document).ready(function () {
+    AOS.init({
+        delay: 50, // values from 0 to 3000, with step 50ms
+        duration: 500
+    });
 
-      $("#navbar").toggleClass("shadow",srcl>50);
+    $(window).scroll(function () {
+        let srcl = $(window).scrollTop();
+
+        $("#navbar").toggleClass("shadow", srcl > 50);
     })
-  })
+})
